2384a3b94a1e4f216681f241fc335f1ed9da055e
emiliegarcen-creator <emilie.garcen@anima.edu.uy>
2026-07-24 09:26:16 -0300

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,48 @@
+const ticket = [
+    {
+      ticketId: 1,
+      descripcion: "Problema con la pantalla",
+      prioridad: "Alta",
+      soporteId: 1234,
+    },
+     {
+      ticketId: 2,
+      descripcion: "No carga",
+      prioridad: "Media",
+      soporteId: 5678,
+    }
+
+]
+
+const cliente = [ 
+    {
+        nombre: "Juan Perez",
+        CI: 12345678,
+        Orientacion: "TIC",
+    },
+        {
+        nombre: "Martin Juan",
+        CI: 234567810,
+        Orientacion: "ADM",
+    }
+]
+
+const maquina = [
+    {
+        maquinaId: 1,
+    },
+        {
+        maquinaId: 2,
+     }
+]
+
+const soporte = [
+    {
+        CIsoporte: 1234,
+        nombre: "Carlos Gomez",
+    },
+    {
+        CIsoporte: 5678,
+        nombre: "Ana Rodriguez",
+    }
+]
