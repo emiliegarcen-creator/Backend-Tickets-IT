ebe5c4680d6fdd8ed9dedbefe718eeca19c8d9db
carolinaortiz-debug <carolina.ortiz@anima.edu.uy>
2026-08-26 08:44:17 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
-//Obtener todos los tickets: 
-GET /tickets
-// Obtener un ticket específico: 
-GET /tickets/{id}
-// Crear un nuevo ticket: 
-POST /tickets
-// Actualizar un ticket existente: 
-PUT /tickets/{id}
-// Actualizar información de un ticket existente: 
-PATCH /tickets/{id}
-// Eliminar un ticket: 
-DELETE /tickets/{id}
-// Obtener los tickets creados por un cliente específico: 
-GET /clientes/{id}/tickets
-// Crear un nuevo ticket para un cliente específico: 
-POST /clientes/{id}/tickets
+const express = require('express')
+const cors = require('cors')
 
-// Obtener la máquina asociada a un ticket: 
-GET /tickets/{id}/maquina
-// Obtener los tickets asignados a un técnico específico: 
-GET /soporte/{id}/tickets
-// Asignar un ticket a un técnico: 
-PUT /tickets/{id}/soporte
-// Obtener los clientes asignados a un técnico específico: 
-GET /soporte/{id}/clientes
-// Obtener la máquina de un cliente específico: 
-GET /clientes/{id}/maquina
-// Actualizar la máquina de un cliente específico: 
-PUT /clientes/{id}/maquina
+const app = express()
+app.use(cors())
+
+const productos = [
+    {id: 1, nombre: 'Remera', precio: 1500},
+    {id: 2, nombre: 'Pantalon', precio: 3000},
+    {id: 3, nombre: 'Zapatos', precio: 2500},
+]
+
+app.get('/productos', (req, res) =>{
+    res.json(productos)
+})
+
+app.listen(3001, () => {
+    console.log('Servidor en http://localhost.3001')
+})
