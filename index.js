aa0360e3c5c022300f42e63fe66dfa9ad43b160e
carolinaortiz-debug <carolina.ortiz@anima.edu.uy>
2026-08-26 09:20:59 -0300

    index.js
    una vez levantado el servidor, para verificar que funcione vayan a la ruta http://localhost:3001/ticket

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 
-const productos = [
-    {id: 1, nombre: 'Remera', precio: 1500},
-    {id: 2, nombre: 'Pantalon', precio: 3000},
-    {id: 3, nombre: 'Zapatos', precio: 2500},
+const ticket = [
+    {id: 1, modelo: 'Acer', descripcion: 'no prende', cargador: 'si', nrolocker: 123},
+    {id: 2, modelo: 'Dell', descripcion: 'no carga', cargador: 'no', nrolocker: 45},
+    {id: 3, modelo: 'HP', descripcion: 'tiene rota la visagra', cargador: 'si', nrolocker: 67},
 ]
 
-app.get('/productos', (req, res) =>{
-    res.json(productos)
+app.get('/ticket', (req, res) =>{
+    console.log('Ruta ticket ejecutada')
+    res.json(ticket)
 })
 
 app.listen(3001, () => {
-    console.log('Servidor en http://localhost.3001')
+    console.log('Servidor en http://localhost:3001')
 })
