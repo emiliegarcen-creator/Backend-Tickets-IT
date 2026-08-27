af7852cae4d73f1b5d52668916071517bff6642b
carolinaortiz-debug <carolina.ortiz@anima.edu.uy>
2026-08-27 12:12:57 -0300

    Delete usuarios-data.js

--- usuarios-data.js
+++
@@ -1,7 +0,0 @@
-let usuarios = [
-    {nombre: 'Lucia Rodriguez', ci: 1234567, curso: 'TIC', mail: 'lucia@gmail.com', contrasena: 'Gatitomiau123?'},
-    {nombre: 'Nicoll Correa', ci: 2345678, curso: 'TIC', mail: 'nicoll@gmail.com', contrasena: 'Perritoguau123?'},
-    {nombre: 'Emilie Garcen', ci: 3456789, curso: 'ADM', mail: 'emilie@gmail.com', contrasena: 'Patitocuack123?'},
-]
-
-module.exports = { usuarios }
