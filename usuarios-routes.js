e39fbc57afeea340df0ca964bb2b483536946de4
carolinaortiz-debug <carolina.ortiz@anima.edu.uy>
2026-08-27 12:13:52 -0300

    Delete usuarios-routes.js

--- usuarios-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller =
-require('../controllers/usuarios-controllers')
-
-router.get('/', controller.listarUsuarios)
-router.get('/:ci', controller.obtenerUsuario)
-
-module.exports = router
