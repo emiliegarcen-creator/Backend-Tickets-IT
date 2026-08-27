185711d199600be70a50fa33d43716da4179d35d
carolinaortiz-debug <carolina.ortiz@anima.edu.uy>
2026-08-27 09:46:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const usuariosRoutes = require('./routes/usuarios-routes')
+
+console.log('Router de usuarios cargado:', usuariosRoutes)
 
 const app = express()
 app.use(cors())
+
+app.use((req, res, next) => {
+    console.log(`Petición recibida: ${req.method} ${req.url}`)
+    next()
+})
 
 const ticket = [
     {id: 1, modelo: 'Acer', descripcion: 'no prende', cargador: 'si', nrolocker: 123},
@@ -14,6 +22,8 @@ app.get('/ticket', (req, res) =>{
     console.log('Ruta ticket ejecutada')
     res.json(ticket)
 })
+
+app.use('/usuarios', usuariosRoutes)
 
 app.listen(3001, () => {
     console.log('Servidor en http://localhost:3001')
