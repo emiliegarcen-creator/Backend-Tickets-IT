468a10e27e90367f4d15e106f99f031063157095
carolinaortiz-debug <carolina.ortiz@anima.edu.uy>
2026-08-28 12:02:58 -0300

    Add files via upload

---
+++ data/maquina-data.js
@@ -0,0 +1,7 @@
+const maquina = [
+    {id: 1, situacion: 'Con dueno', cargador: 'Si', dueno: ' '},
+    {id: 2, situacion: 'Prestamo', cargador: 'No', dueno: ' '},
+    {id: 3, situacion: 'Sin uso', cargador: 'Si', dueno: ' '},
+]
+
+module.exports = { maquina }
