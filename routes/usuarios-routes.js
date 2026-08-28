4884ac181b44bf4ebd9bf1a0f2b6a2a33da68d59
carolinaortiz-debug <carolina.ortiz@anima.edu.uy>
2026-08-28 12:04:15 -0300

    Update usuarios-routes.js

--- routes/usuarios-routes.js
+++ routes/usuarios-routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const controller =
-require('../controllers/usuarios-controllers')
+const controller = require('../controllers/usuarios-controllers')
+const ticketController = require('../controllers/ticket-controllers')
 
 router.get('/', controller.listarUsuarios)
 router.get('/:ci', controller.obtenerUsuario)
+router.get('/:id/ticket', ticketController.listarTicketsPorUsuario)
+router.post('/:id/ticket', ticketController.crearTicket)
 
 module.exports = router
