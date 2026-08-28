57a29629304ec011826c53edf83806f458ff2120
carolinaortiz-debug <carolina.ortiz@anima.edu.uy>
2026-08-28 12:00:49 -0300

    Update usuarios-data.js

--- data/usuarios-data.js
+++ data/usuarios-data.js
@@ -1,7 +1,7 @@
 let usuarios = [
-    {nombre: 'Lucia Rodriguez', ci: 1234567, curso: 'TIC', mail: 'lucia@gmail.com', contrasena: 'Gatitomiau123?'},
-    {nombre: 'Nicoll Correa', ci: 2345678, curso: 'TIC', mail: 'nicoll@gmail.com', contrasena: 'Perritoguau123?'},
-    {nombre: 'Emilie Garcen', ci: 3456789, curso: 'ADM', mail: 'emilie@gmail.com', contrasena: 'Patitocuack123?'},
+    {nombre: 'Lucia Rodriguez', ci: 1234567, rol: 'Soporte', mail: 'lucia@gmail.com', contrasena: 'Gatitomiau123?'},
+    {nombre: 'Nicoll Correa', ci: 2345678, rol: 'Funcionario', mail: 'nicoll@gmail.com', contrasena: 'Perritoguau123?'},
+    {nombre: 'Emilie Garcen', ci: 3456789, rol: 'Estudiante', mail: 'emilie@gmail.com', contrasena: 'Patitocuack123?'},
 ]
 
 module.exports = { usuarios }
