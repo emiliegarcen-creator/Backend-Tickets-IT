b27489566b77078a0be06df93e43fc7413db9a48
carolinaortiz-debug <carolina.ortiz@anima.edu.uy>
2026-08-31 15:19:12 -0300

    Update ticket-controllers.js

--- controllers/ticket-controllers.js
+++ controllers/ticket-controllers.js
@@ -1,5 +1,6 @@
 const data = require('../data/ticket-data')
 const usuariosData = require('../data/usuarios-data')
+const maquinaData = require('../data/maquina-data')
 
 function listarTickets(req, res) {
     res.json(data.ticket)
@@ -31,15 +32,20 @@ function actualizarPrioridad(req, res) {
 
 function crearTicket(req, res) {
     const { id } = req.params
-    const { modelo, descripcion, cargador, nrolocker } = req.body
+    const { descripcion, nrolocker, maquina } = req.body
 
     const usuarioExiste = usuariosData.usuarios.find(u => u.ci == id)
     if (!usuarioExiste) {
         return res.status(404).json({ error: 'Usuario no encontrado' })
     }
 
-    if (!modelo || !descripcion || !cargador || !nrolocker) {
-        return res.status(400).json({ error: 'Faltan datos obligatorios (modelo, descripcion, cargador, nrolocker)' })
+    if (!descripcion || !nrolocker || !maquina) {
+        return res.status(400).json({ error: 'Faltan datos obligatorios (descripcion, nrolocker, maquina)' })
+    }
+
+    const maquinaExiste = maquinaData.maquina.find(m => m.id == maquina)
+    if (!maquinaExiste) {
+        return res.status(404).json({ error: 'Máquina no encontrada' })
     }
 
     const nuevoId = data.ticket.length > 0
@@ -48,12 +54,11 @@ function crearTicket(req, res) {
 
     const nuevoTicket = {
         id: nuevoId,
-        modelo,
         descripcion,
-        cargador,
         nrolocker,
         prioridad: 'Media',
-        ci: id
+        ci: id,
+        maquina
     }
 
     data.ticket.push(nuevoTicket)
@@ -74,8 +79,44 @@ function eliminarTicket(req, res) {
 
 function listarTicketsPorUsuario(req, res) {
     const { id } = req.params
+
+     const usuarioExiste = usuariosData.usuarios.find(u => u.ci == id)
+    if (!usuarioExiste) {
+        return res.status(404).json({ error: 'Usuario no encontrado' })
+    }
+    
     const ticketsDelUsuario = data.ticket.filter(t => t.ci == id)
     res.json(ticketsDelUsuario)
 }
 
-module.exports = { listarTickets, obtenerTickets, actualizarPrioridad, crearTicket, eliminarTicket, listarTicketsPorUsuario }
+function listarTicketsMaquina(req, res) {
+    const { id } = req.params
+    const ticketsDeLaMaquina = data.ticket.filter(t => t.ci == id)
+    res.json(ticketsDelUsuario)
+}
+
+function obtenerMaquinaDeTicket(req, res) {
+    const { id } = req.params
+
+    const ticketExistente = data.ticket.find(t => t.id == id)
+    if (!ticketExistente) {
+        return res.status(404).json({ error: 'Ticket no encontrado' })
+    }
+
+    const maquinaDelTicket = maquinaData.maquina.find(m => m.id == ticketExistente.maquina)
+    if (!maquinaDelTicket) {
+        return res.status(404).json({ error: 'Máquina no encontrada' })
+    }
+
+    res.json(maquinaDelTicket)
+}
+
+module.exports = { 
+    listarTickets,
+    obtenerTickets,
+    actualizarPrioridad,
+    crearTicket, 
+    eliminarTicket, 
+    listarTicketsPorUsuario,
+    obtenerMaquinaDeTicket
+}
