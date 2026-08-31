adeae30cde415d2f6e6d43f7e6be99e1eea61f3a
carolinaortiz-debug <carolina.ortiz@anima.edu.uy>
2026-08-31 15:18:31 -0300

    Update maquina-controllers.js

--- controllers/maquina-controllers.js
+++ controllers/maquina-controllers.js
@@ -11,4 +11,24 @@ function obtenerMaquinas(req, res) {
     res.json(u)
 }
 
-module.exports = { listarMaquinas, obtenerMaquinas }
+function actualizarMaquina(req, res) {
+    const { id } = req.params
+    const { situacion, dueno, cargador } = req.body
+
+    const maquinaExistente = data.maquina.find(m => m.id == id)
+    if (!maquinaExistente) {
+        return res.status(404).json({ error: 'Máquina no encontrada' })
+    }
+
+    if (!situacion || !dueno || !cargador) {
+        return res.status(400).json({ error: 'Todos los campos son obligatorios (situacion, dueno, cargador)' })
+    }
+
+    maquinaExistente.situacion = situacion
+    maquinaExistente.dueno = dueno
+    maquinaExistente.cargador = cargador
+
+    res.json(maquinaExistente)
+}
+
+module.exports = { listarMaquinas, obtenerMaquinas, actualizarMaquina }
