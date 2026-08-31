0178aa7da4b3f4825a076a958f9a84d703fae231
carolinaortiz-debug <carolina.ortiz@anima.edu.uy>
2026-08-31 15:24:03 -0300

    Update ticket-data.js

--- data/ticket-data.js
+++ data/ticket-data.js
@@ -1,7 +1,7 @@
 const ticket = [
-    {id: 1, modelo: 'Acer', descripcion: 'no prende', cargador: 'si', nrolocker: 123, prioridad: 'Alta', ci: 1234567},
-    {id: 2, modelo: 'Dell', descripcion: 'no carga', cargador: 'no', nrolocker: 45, prioridad: 'Media', ci: 2345678},
-    {id: 3, modelo: 'HP', descripcion: 'tiene rota la visagra', cargador: 'si', nrolocker: 67, prioridad: 'Baja', ci: 3456789},
+    {id: 1, descripcion: 'no prende', nrolocker: 123, prioridad: 'Alta', ci: 1234567, maquina: 1},
+    {id: 2, descripcion: 'no carga', nrolocker: 45, prioridad: 'Media', ci: 2345678, maquina: 2},
+    {id: 3, descripcion: 'tiene rota la visagra', nrolocker: 67, prioridad: 'Baja', ci: 3456789, maquina: 3},
 ]
 
 module.exports = { ticket }
