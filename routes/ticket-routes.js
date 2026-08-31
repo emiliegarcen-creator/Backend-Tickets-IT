606f0449d7ab581ade2f8a40ee669b1160086cb2
carolinaortiz-debug <carolina.ortiz@anima.edu.uy>
2026-08-31 15:20:08 -0300

    Update ticket-routes.js

--- routes/ticket-routes.js
+++ routes/ticket-routes.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const controller =
-require('../controllers/ticket-controllers')
+const controller = require('../controllers/ticket-controllers')
 
 router.get('/', controller.listarTickets)
 router.get('/:id', controller.obtenerTickets)
+router.get('/:id/maquina', controller.obtenerMaquinaDeTicket)
 router.post('/', controller.crearTicket)
 router.patch('/:id', controller.actualizarPrioridad)
 router.delete('/:id', controller.eliminarTicket)
